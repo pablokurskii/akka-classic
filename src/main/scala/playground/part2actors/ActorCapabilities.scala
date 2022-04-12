5ed896b6d8831d52e26fb6f9e1239b7c0e39de0e
Pablo Kurskii <pablo.kurskii@gmail.com>
2022-04-12 12:31:24 +0200

    Update ActorCapabilities.scala

--- src/main/scala/playground/part2actors/ActorCapabilities.scala
+++ src/main/scala/playground/part2actors/ActorCapabilities.scala
@@ -5,31 +5,32 @@ import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 object ActorCapabilities extends App {
   class SimpleActor extends Actor {
     override def receive: Receive = {
-      case "Hi!" => context.sender() ! "Hello, there!" //replying to message
-      case message: String => println(s"[$self] I have received $message")
+      case "Hi!" => sender() ! s"$self replied to $sender Hello, there!" //replying to message
+      case message: String => println(s"$message")
       case number: Int => println(s"[simple actor] I have received a NUMBER $number")
       case SpeciaMessage(contents) => println(s"[simple actor] I have received a SPECIAL $contents")
       case SendMessageToYourself(content) =>
         self ! content
       case SayHiTo(ref) => ref ! "Hi!"
+      case WiPhoneMessage(content, ref) => ref forward (content + "s")
     }
   }
 
   val system = ActorSystem("actorCapabilitiesDemo")
   val simpleActor: ActorRef = system.actorOf(Props[SimpleActor], "simpleActor")
 
-  simpleActor ! "hello, actor"
+//  simpleActor ! "hello, actor"
 
   // 1- messages can be of any type
-  simpleActor ! 42
+//  simpleActor ! 42
 
   case class SpeciaMessage(contents: String)
 
-  simpleActor ! SpeciaMessage("some special content")
+//  simpleActor ! SpeciaMessage("some special content")
 
   case class SendMessageToYourself(content: String)
 
-  simpleActor ! SendMessageToYourself("I am an actor")
+//  simpleActor ! SendMessageToYourself("I am an actor")
 
   //  3- actors can reply to messages
   val alice = system.actorOf(Props[SimpleActor], "alice")
@@ -38,4 +39,7 @@ object ActorCapabilities extends App {
   case class SayHiTo(ref: ActorRef)
 
   alice ! SayHiTo(bob)
+
+  case class WiPhoneMessage(content: String, ref: ActorRef)
+  alice ! WiPhoneMessage("Hi", bob)
 }
